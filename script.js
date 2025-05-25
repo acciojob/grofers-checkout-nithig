d79b3020ce198ce24667ba0093e06b1a3730a98e
nithig <154483033+nithig@users.noreply.github.com>
2025-05-25 20:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
 //Add your code here
-  
+	const prices = document.querySelectorAll('.price');
+	let total = 0;
+	prices.forEach((i) => {
+		total += Number(i.textContent)
+	})
+	const totRow = document.createElement("tr");
+	const totCell = document.createElement("td");
+	totCell.textContent = "Total";
+	const totDat = document.createElement("td");
+	totDat.textContent = total;
+	totRow.appendChild(totCell);
+	totRow.appendChild(totDat);
+	document.getElementById('cart').appendChild(totRow);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
